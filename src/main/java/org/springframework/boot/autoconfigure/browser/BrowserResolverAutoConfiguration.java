921fe06f5c4f072226bde7edb15da72ee026f8ae
abaudet <abaudet@sii.fr>
2015-03-28 14:18:37 +0100

    - Add properties auto-configuration

--- src/main/java/org/springframework/boot/autoconfigure/browser/BrowserResolverAutoConfiguration.java
+++ src/main/java/org/springframework/boot/autoconfigure/browser/BrowserResolverAutoConfiguration.java
@@ -10,10 +10,18 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.browser.config.BrowserMappingConfiguration;
 import org.springframework.browser.mvc.BrowserHandlerMethodArgumentResolver;
 import org.springframework.browser.mvc.BrowserResolverHandlerInterceptor;
+import org.springframework.browser.resolver.HttpRequestBrowserResolver;
+import org.springframework.browser.resolver.ConfigurableBrowserResolver;
+import org.springframework.browser.resolver.UserAgentBrowserResolver;
+import org.springframework.browser.resolver.UserAgentHttpHeaderDelegate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -31,15 +39,33 @@ public class BrowserResolverAutoConfiguration {
 
 	@Configuration
 	@ConditionalOnWebApplication
+	@PropertySource(value="classpath:/browsers.properties")
+	@EnableConfigurationProperties(BrowserMappingConfiguration.class)
 	protected static class BrowserResolverMvcConfiguration extends WebMvcConfigurerAdapter {
 
 		@Autowired
 		private BrowserResolverHandlerInterceptor browserResolverHandlerInterceptor;
 
 		@Bean
+		@ConfigurationProperties(prefix="spring")
+		public BrowserMappingConfiguration browserMappingConfiguration() {
+			return new BrowserMappingConfiguration();
+		}
+
+		@Bean
+		public UserAgentBrowserResolver userAgentBrowserResolver() {
+			return new ConfigurableBrowserResolver(browserMappingConfiguration());
+		}
+		
+		@Bean
+		public HttpRequestBrowserResolver httpRequestBrowserResolver() {
+			return new UserAgentHttpHeaderDelegate(userAgentBrowserResolver());
+		}
+
+		@Bean
 		@ConditionalOnMissingBean(BrowserResolverHandlerInterceptor.class)
 		public BrowserResolverHandlerInterceptor browserResolverHandlerInterceptor() throws IOException {
-			return new BrowserResolverHandlerInterceptor();
+			return new BrowserResolverHandlerInterceptor(httpRequestBrowserResolver());
 		}
 
 		@Bean
